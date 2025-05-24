74ffbd68bbb87454bdbf8ac6f5f26a4e3cbed4ad
JulioRB9 <julio.rivera.1596@gmail.com>
2025-05-24 00:08:21 -0600

    App Contactos

--- ModuloMaster/settings.py
+++ ModuloMaster/settings.py
@@ -130,3 +130,13 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configuracion de correo electronico
+EMAIL_BACKEND = 'django.core.outlook.EmailBackend'
+EMAIL_HOST = 'smtp-mail.outlook.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = '?'  # Reemplaza con tu correo electrónico
+EMAIL_HOST_PASSWORD = '?'  # Reemplaza con tu contraseña de correo electrónico
+
+
--- contactos/views.py
+++ contactos/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .forms import formularioContacto
 from django.shortcuts import redirect
+from django.core.mail import EmailMessage
 
 # Create your views here.
 # https://www.youtube.com/watch?v=_BOOv6GK-68&list=PLU8oAlHdN5BmfvwxFO7HdPciOCmmYneAB&index=45
@@ -16,6 +17,18 @@ def contacto(request):
             email = request.POST.get("email")
             mensaje = request.POST.get("mensaje")
             # print(f"Nombre: {nombre}, Email: {email}, Mensaje: {mensaje}")
-            return redirect("/contactos/?ok")
+
+            # Enviar correo electrónico
+            email = EmailMessage(
+                "Nuevo mensaje de contacto de APP django",
+                "El usuario {} con email {} ha enviado el siguiente mensaje:\n\n{}".format(
+                    nombre, email, mensaje
+                ),["julio.rivera.1596@outlook.com"],reply_to=[email])
+                
+            try:
+                email.send()
+                return redirect("/contactos/?ok")
+            except:
+                return redirect("/contactos/?fail")
 
     return render(request, "contactos/contacto.html", {"miformulario": formulario})
