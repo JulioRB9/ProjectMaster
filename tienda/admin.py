e31fd3539a392f054a8760d5777233fb9d87b4b4
JulioRB9 <julio.rivera.1596@gmail.com>
2025-06-14 08:12:53 -0600

    Tienda

--- tienda/admin.py
+++ tienda/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import CategoriaProd, Producto
 # Register your models here.
+
+class CategoriaProdAdmin(admin.ModelAdmin):
+    readonly_fields = ('create', 'update')
+
+class ProductoAdmin(admin.ModelAdmin):
+    readonly_fields = ('create', 'update')
+
+# Registra el modelo en el panel de adminstracion.
+admin.site.register(CategoriaProd, CategoriaProdAdmin)
+admin.site.register(Producto, ProductoAdmin)
+
+
--- tienda/models.py
+++ tienda/models.py
@@ -18,10 +18,15 @@ class Producto(models.Model):
     nombre = models.CharField(max_length=50)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     categoria = models.ForeignKey(CategoriaProd, on_delete=models.CASCADE)
-    img = models.ImageField(upload_to='productos', null=True, blank=True)
+    imagen = models.ImageField(upload_to='productos', null=True, blank=True)
     disponibilidad = models.BooleanField(default=True)
+    create = models.DateField(auto_now_add=True)
+    update = models.DateField(auto_now=True)
 
 
     class Meta:
         verbose_name = 'Producto'
         verbose_name_plural = 'Productos'
+    
+    def __str__(self):
+        return self.nombre
